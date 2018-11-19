90a75d767f9dcb27226dee3e29787e127138344a
Dani <dgilromero@bloomberg.net>
2018-11-19 16:26:19 +0000

    Added bootstrap and jobs are showing up in home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    # Title
+    title = models.CharField(max_length=60)
+    # Publication date
+    pub_date = models.DateTimeField()
+    # Body
+    body = models.TextField()
+    # Image
+    image = models.ImageField(upload_to='images/')
