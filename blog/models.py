f114ecd7f0a25307b3289a6bcd68b954c50558dd
Dani <dgilromero@bloomberg.net>
2018-11-19 18:22:58 +0000

    Added home, detail and almost polished the whole project

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,13 @@ class Blog(models.Model):
     body = models.TextField()
     # Image
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+    # Return certain portion of thje summary
+
+    def body_summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
